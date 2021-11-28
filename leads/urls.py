1c08ef08a56da9b9994b44f8d2695eb4cb61cd2f
Connor Cash <cashconnor@protonmail.com>
2021-11-27 22:22:11 -0500

    Detail list, create lead

---
+++ leads/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import lead_detail, lead_list, lead_create
+
+app_name = "leads"
+
+urlpatterns = [
+    path('', lead_list),
+    path('create/', lead_create),
+    path('<int:pk>/', lead_detail),
+]
